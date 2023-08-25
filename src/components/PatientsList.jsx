7da504a14df4ce8c04537d19927892bdeade65e0
nancy <smithnancy@outlook.com>
2023-08-25 16:03:00 +1000

    display text  depent on condition

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,17 +1,27 @@
 import Patient from "./Patient"
 const PatientsList = ({ patients }) => {
+
+  //console.log(patients && patients.length);
   return (
     <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
-      <h2 className="text-center font-black text-3xl">Patients List</h2>
-      <p className="text-xl text-center mt-5 mb-10">Manage{' '}<span className="text-indigo-600 font-bold">Patiensts</span></p>
-      
-      {patients.map((patient) => (
-        <Patient
-          key={patient.id}
-          patient={patient}
-        />
-       ))}
-      
+      {patients && patients.length ? (
+      <>
+        <h2 className="text-center font-black text-3xl">Patients List</h2>
+        <p className="text-xl text-center mt-5 mb-10">Manage{' '}<span className="text-indigo-600 font-bold">Patiensts</span></p>
+
+        {patients.map((patient) => (
+          <Patient
+            key={patient.id}
+            patient={patient}
+          />
+        ))}
+      </>
+      ):(
+        <>
+          <h2 className="text-center font-black text-3xl">No Patients </h2>
+          <p className="text-xl text-center mt-5 mb-10">Manage{' '}<span className="text-indigo-600 font-bold">Add Patiensts</span></p>
+        </>
+      )}
     </div>
   )
 }
